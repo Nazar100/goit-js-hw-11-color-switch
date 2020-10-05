f23ae7c9ce195a5b72932efb9e5513590b106279
Nazar100 <leshc005@umn.edu>
2020-10-05 14:11:00 +0300

    fixes

--- src/js/app.js
+++ src/js/app.js
@@ -17,21 +17,22 @@ const bodyRef = document.querySelector('body');
 
 startBtnRef.addEventListener('click', changeColor);
 
+let interval = null;
+
 function changeColor() {
   stopBtnRef.addEventListener('click', stopColor);
-  const interval = setInterval(randomColor, 1000);
+  interval = setInterval(randomColor, 1000);
   startBtnRef.disabled = true;
+}
 
-  function randomColor() {
-    const color = colors[randomIntegerFromInterval(0, colors.length - 1)];
-    bodyRef.style.backgroundColor = `${color}`;
-    console.log(color);
-  }
+function randomColor() {
+  const color = colors[randomIntegerFromInterval(0, colors.length - 1)];
+  bodyRef.style.backgroundColor = `${color}`;
+}
 
-  function stopColor() {
-    clearInterval(interval);
-    startBtnRef.disabled = false;
-  }
+function stopColor() {
+  clearInterval(interval);
+  startBtnRef.disabled = false;
 }
 
 const randomIntegerFromInterval = (min, max) => {
